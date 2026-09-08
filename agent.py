e4cc1b63ce72ca51436b591268eb09a1a13a5090
sakxshiborkar <sakxshi.borkar@gmail.com>
2026-09-08 13:21:05 +0400

    agent.py

--- agent.py
+++ agent.py
@@ -9,7 +9,7 @@ client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 
 def ask_claude(prompt) :
     response = client.chat.completions.create(
-        model="llama-3.3-70b-versatile",
+        model="openai/gpt-oss-120b",
         max_tokens=500,
         messages=[
             {"role": "user" , "content": prompt}
